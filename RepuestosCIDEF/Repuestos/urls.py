da75c3ef38f1c0fd63c9b3e8c2b973be02b3381e
Junisses <junissesamanda.03@gmail.com>
2021-01-27 16:08:30 -0300

    Index pagina

---
+++ RepuestosCIDEF/Repuestos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+#from django.conf import settings
+#from django.conf.urls.static import static
+#from .views import ProductoViewSet
+#from rest_framework import routers
+
+
+urlpatterns = [
+    path('',views.inicio, name='inicio'),
+]
