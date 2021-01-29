5422799fc4b3d5cac112178b1a90b8ae7c2c9ac7
Junisses <junissesamanda.03@gmail.com>
2021-01-29 12:20:42 -0300

    Models, CRUD, base de datos

--- RepuestosCIDEF/Repuestos/admin.py
+++ RepuestosCIDEF/Repuestos/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import MarcaAuto
+from .models import Categoria
+from .models import Repuesto
+from .models import Modelo
+from .models import Partes
 # Register your models here.
+
+class marcaAutoAdmin(admin.ModelAdmin):
+    list_display   = ['nombre'] 
+admin.site.register(MarcaAuto, marcaAutoAdmin)
+
+class categoriaAdmin(admin.ModelAdmin):
+    list_display   = ['nombre'] 
+admin.site.register(Categoria, categoriaAdmin)
+
+class repuestoAdmin(admin.ModelAdmin):
+    list_display   = ['nombreRepuesto'] 
+admin.site.register(Repuesto, repuestoAdmin)
+
+class modeloAdmin(admin.ModelAdmin):
+    list_display   = ['nombre'] 
+admin.site.register(Modelo, modeloAdmin)
+
+class parteAdmin(admin.ModelAdmin):
+    list_display   = ['nombre'] 
+admin.site.register(Partes, parteAdmin)
--- RepuestosCIDEF/Repuestos/models.py
+++ RepuestosCIDEF/Repuestos/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class MarcaAuto(models.Model):
+    nombre        = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Modelo(models.Model):
+    marca         = models.ForeignKey(MarcaAuto, blank=True, null=True, on_delete=models.SET_NULL)
+    nombre        = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre        = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Repuesto(models.Model):
+    marca            = models.ForeignKey(MarcaAuto, blank=True, null=True, on_delete=models.SET_NULL)
+    categoria        = models.ForeignKey(Categoria, blank=True, null=True, on_delete=models.SET_NULL)
+    modelo           = models.ForeignKey(Modelo, blank=True, null=True, on_delete=models.SET_NULL)
+    nombreRepuesto   = models.TextField(max_length=60)
+    codigo           = models.DecimalField(max_digits=13, decimal_places=0)
+    descripcion      = models.TextField(max_length = 200)
+    stock            = models.IntegerField()
+    precioVenta      = models.IntegerField()
+
+    def __str__(self):
+        return self.nombreRepuesto
+
+class Partes(models.Model):
+    nombreRepuesto    = models.ForeignKey(Repuesto, blank=True, null=True, on_delete=models.SET_NULL)   
+    nombre            = models.TextField(max_length=60)
+    codigo            = models.DecimalField(max_digits=13, decimal_places=0)
+    descripcion       = models.TextField(max_length = 200)
+    stock             = models.IntegerField()
+    precioVenta       = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
