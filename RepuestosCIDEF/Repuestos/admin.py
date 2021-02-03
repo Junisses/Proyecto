476272245e418b35956e62128c65d271074582f0
Junisses <junissesamanda.03@gmail.com>
2021-02-03 11:17:07 -0300

    Comunas y localidades

--- RepuestosCIDEF/Repuestos/admin.py
+++ RepuestosCIDEF/Repuestos/admin.py
@@ -4,6 +4,8 @@ from .models import Categoria
 from .models import Repuesto
 from .models import Modelo
 from .models import Partes
+from .models import Localidades
+from .models import Comuna
 # Register your models here.
 
 class marcaAutoAdmin(admin.ModelAdmin):
@@ -25,3 +27,11 @@ admin.site.register(Modelo, modeloAdmin)
 class parteAdmin(admin.ModelAdmin):
     list_display   = ['nombre'] 
 admin.site.register(Partes, parteAdmin)
+
+class localidadAdmin(admin.ModelAdmin):
+    list_display   = ['nombre'] 
+admin.site.register(Localidades, localidadAdmin)
+
+class comunaAdmin(admin.ModelAdmin):
+    list_display   = ['nombre'] 
+admin.site.register(Comuna, comunaAdmin)
