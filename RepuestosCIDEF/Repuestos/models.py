3db00529f691b53f6cb941d72638b0880c5cb26d
Junisses <junissesamanda.03@gmail.com>
2021-02-11 11:00:08 -0300

    Cambio atributos models

--- RepuestosCIDEF/Repuestos/models.py
+++ RepuestosCIDEF/Repuestos/models.py
@@ -13,8 +13,17 @@ class MarcaAuto(models.Model):
     def __str__(self):
         return self.nombre
 
+class Modelo(models.Model):
+    marca         = models.ForeignKey(MarcaAuto, blank=True, null=True, on_delete=models.SET_NULL)
+    nombre        = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
 class Repuesto(models.Model):
     categoria        = models.ForeignKey(Categoria, blank=True, null=True, on_delete=models.SET_NULL)
+    marca            = models.ForeignKey(MarcaAuto, blank=True, null=True, on_delete=models.SET_NULL)
+    modelo           = models.ForeignKey(Modelo, blank=True, null=True, on_delete=models.SET_NULL)
     nombreRepuesto   = models.TextField(max_length=60)
     codigo           = models.DecimalField(max_digits=13, decimal_places=0)
     descripcion      = models.TextField(max_length = 200)
@@ -24,9 +33,3 @@ class Repuesto(models.Model):
     def __str__(self):
         return self.nombreRepuesto
      
-class Modelo(models.Model):
-    marca         = models.ForeignKey(MarcaAuto, blank=True, null=True, on_delete=models.SET_NULL)
-    nombre        = models.TextField(max_length=50)
-
-    def __str__(self):
-        return self.nombre
