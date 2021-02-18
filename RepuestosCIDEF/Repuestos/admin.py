bdf03d0f70502edb3442add15cc16c3b39e223bc
Junisses <junissesamanda.03@gmail.com>
2021-02-18 13:55:23 -0300

    Aplicacion de descripcion para el Admin

--- RepuestosCIDEF/Repuestos/admin.py
+++ RepuestosCIDEF/Repuestos/admin.py
@@ -13,14 +13,21 @@ class categoriaAdmin(admin.ModelAdmin):
 admin.site.register(Categoria, categoriaAdmin)
 
 class repuestoAdmin(admin.ModelAdmin):
-    list_display   = ['nombreRepuesto'] 
+    list_display   = ['nombreRepuesto',
+                      'categoria',
+                      'tipo',
+                      'marca',
+                      'modelo',
+                      'codigo'] 
 admin.site.register(Repuesto, repuestoAdmin)
 
 class modeloAdmin(admin.ModelAdmin):
-    list_display   = ['nombre'] 
+    list_display   = ['nombre',
+                      'marca']
 admin.site.register(Modelo, modeloAdmin)
 
 class tipoAdmin(admin.ModelAdmin):
-    list_display   = ['nombre'] 
+    list_display   = ['nombre',
+                      'categoria'] 
 admin.site.register(Tipo, tipoAdmin)
 
